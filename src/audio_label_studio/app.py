6cb54b3cd5f7b2f6617f5d052841afdacb7adc43
tolatolatop <tolatolatop@gmail.com>
2025-05-10 03:14:05 +0800

    feat: 支持读取环境

--- src/audio_label_studio/app.py
+++ src/audio_label_studio/app.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
 from .ocr_predict import router as ocr_router
+
+load_dotenv('.env.prod')
 
 app = FastAPI()
 app.include_router(ocr_router)
